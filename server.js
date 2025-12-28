c97028617c00e6e11002de7f27cf090322b35fc4
npask <87610771+npask@users.noreply.github.com>
2025-12-28 18:44:26 +0100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, { cors: { origin: "*" } });
+
+app.use(cors());
+app.use(express.json());
+
+let players = {};
+
+/* Roblox Positions */
+app.post("/positions", (req, res) => {
+  req.body.players?.forEach(p => {
+    players[p.userId] = { ...p, lastSeen: Date.now() };
+  });
+  res.sendStatus(200);
+});
+
+app.get("/players", (_, res) => res.json(players));
+
+/* Voice */
+io.on("connection", socket => {
+  console.log("Voice client 🎧", socket.id);
+
+  socket.on("join", userId => {
+    socket.userId = userId;
+  });
+
+  socket.on("voice", data => {
+    // data: { from, audio }
+    console.log("tramsmitting from: " + data.from)
+    socket.broadcast.emit("voice", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("Client weg 💀");
+  });
+});
+
+server.listen(3000, () => {
+  console.log("Server läuft auf 3000 🚀");
+});
